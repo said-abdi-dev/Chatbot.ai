4261850daf5937002dd14c8fd490477900452422
Jeff Muter <muterjeffery@gmail.com>
2023-08-13 15:59:20 -0400

    push frontend changes

--- vue/src/components/ChatBot.vue
+++ vue/src/components/ChatBot.vue
@@ -58,15 +58,28 @@
             :class="{ 'slider-on': switchValue, 'slider-off': !switchValue }"
           ></span>
         </span> -->
-        <button @click="startRecognition">Speak</button>
-         <button v-if="messages.length != 0" @click="speakResponse">Listen</button>
-         <button v-if="messages.length != 0" @click="stopSpeaking">Stop</button>
+        <div class="btn-wrapper" v-if="messages.length != 0" @click="startRecognition">
+          <svg class="btn-standard" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
+          </svg>
+        </div>
+        <!-- <button /> -->
+        <div class="btn-wrapper" v-if="messages.length != 0" @click="startRecognition">
+          <svg class="btn-standard" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />
+          </svg>
+        </div>
+        <div class="btn-wrapper"  v-if="messages.length != 0" @click="stopSpeaking">
+          <svg class="btn-standard" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
+          </svg>
+        </div>
       </div>
     </section>
     <div class="chat-inputs">
       <input type="text" v-model="message" @keyup.enter="sendMessage" />
-      <button class="send-button" @click="sendMessage" :disabled="message.trim() === ''">
-        <svg class="send-btn-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+      <button class="btn-wrapper" @click="sendMessage" :disabled="message.trim() === ''">
+        <svg class="btn-standard" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>
       </button>
@@ -83,7 +96,7 @@ export default {
 
   data() {
     return {
-      cat: "4",
+            cat: "4",
       message: "",
       messages: [], //all the messages in an array
       responseArrayFromServer: [], //remember to RENAME this later
@@ -192,9 +205,9 @@ export default {
       });
 
       this.message = "";
-      
+
       //probably a good idea to have this if condition in a different method
-       if (message.includes("job")) {
+      if (message.includes("job")) {
         LinkedInService.getJob(message).then((response) => {
           console.log(response.data.data[0].url);
           let linkedJob =
@@ -441,8 +454,8 @@ button {
 }
 
 .send-btn-svg {
-  height: 2rem;
-  width: 2rem;
+  height: 1.75rem;
+  width: 1.75rem;
 }
 .send-button {
   height: 4rem;
@@ -452,9 +465,6 @@ button {
   margin-left: 1rem;
   margin-right: 1rem;
   border-radius: 50%;
-}
-.voiceAndText {
-  padding-top: 200;
 }
 .switch {
   display: inline-block;
@@ -481,4 +491,32 @@ button {
 .slider-off {
   transform: translateX(0);
 }
+
+.voiceAndText {
+  padding-top: 200;
+  display: flex;
+}
+
+.btn-wrapper {
+  height: 3.5rem;
+  width: 3.5rem;
+    margin-top: 1rem;
+  margin-bottom: 1rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+    background-color: #0070c8;
+  border-radius: 50%;
+  display: flex; /* Use flexbox to create a flexible container */
+  justify-content: center; /* Center horizontally */
+  align-items: center; /* Center vertically */
+  cursor: pointer;
+}
+
+.btn-standard {
+  height: 1.75rem;
+  width: 1.75rem;
+  border-radius: 50%;
+  background-color: #0070c8;
+  color: white
+  }
 </style>
