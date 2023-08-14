61b19e119c0f002a1c10bdefd58b99193c15c803
Noah Olsen <noah23olsen@gmail.com>
2023-08-14 09:06:59 -0400

    pushing

--- vue/src/components/ChatBot.vue
+++ vue/src/components/ChatBot.vue
@@ -65,7 +65,7 @@
     </section>
     <div class="chat-inputs">
       <input type="text" v-model="message" @keyup.enter="sendMessage" />
-      <button class="send-button" @click="sendMessage" :disabled="message.trim() === ''">
+      <button class="send-button" @click="sendMessage">
         <svg class="send-btn-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>
